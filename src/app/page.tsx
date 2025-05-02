cef4be216d0869d5b2cb04229d4d7db9a3049658
René Schober <r.schober@outlook.com>
2025-05-02 21:18:56 +0200

    Adding Styling to tickets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-import { ticketsPath } from "@/path";
-import Link from "next/link";
+import { ticketsPath } from '@/path';
+import Link from 'next/link';
 
 const HomePage = () => {
-  return <div>
-    <h2 className="text-lg">HomePage</h2>
-    <Link href={ticketsPath()} className="text-sm underline">Go to Tickets</Link>
-  </div>
-}
+  return (
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Home</h2>
+        <p className="text-sm text-muted">Your home place to start</p>
+      </div>
+      <div className="flex-1 flex flex-col items-center">
+        <Link href={ticketsPath()} className="text-sm underline">
+          Go to Tickets
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,25 +1,39 @@
-import { intitialTickets } from "@/data";
-import { ticketPath } from "@/path";
-import Link from "next/link";
+import { intitialTickets } from '@/data';
+import { ticketPath } from '@/path';
+import Link from 'next/link';
 
 const TICKET_ICONS = {
-    OPEN: "O",
-    DONE: "X",
-    IN_PROGRESS: ">"
-}
+  OPEN: 'O',
+  DONE: 'X',
+  IN_PROGRESS: '>',
+};
 
 const TicketsPage = () => {
-    return (
-        <div>
-            {intitialTickets.map((ticket) => (
-                <div key={ticket.id}>
-                    <div>{TICKET_ICONS[ticket.status]}</div>
-                    <h2 className="text-lg">{ticket.title}</h2>
-                    <Link href={ticketPath(ticket.id)} className="text-sm underline">View</Link>
-                </div>
-            ))}
-        </div>
-    )
-}
+  return (
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3l font-bold tracking-tight">Tickets</h2>
+        <p className="text-sm text-muted-foreground">
+          All your ticktes at one place
+        </p>
+      </div>
+      <div className="flex-1 flex flex-col items-center pag-y-4">
+        {intitialTickets.map((ticket) => (
+          <div key={ticket.id} className='w-full max-w-[420px] p-4 border border-slate-100 rounded'>
+            <div>{TICKET_ICONS[ticket.status]}</div>
+            <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
+            <p className="text-sm text-slate-400 truncate">{ticket.content}</p>
+            <Link
+              href={ticketPath(ticket.id)}
+              className="text-sm underline"
+            >
+              View
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default TicketsPage;
