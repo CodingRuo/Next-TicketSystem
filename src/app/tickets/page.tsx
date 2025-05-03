eec7fdec07283eb369dac1cfa23d3c803d785ef5
René Schober <r.schober@outlook.com>
2025-05-03 19:39:01 +0200

    Create data fetching

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,25 +1,21 @@
 import { Heading } from '@/components/heading';
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import { Separator } from '@/components/ui/separator';
-import { intitialTickets } from '@/data';
 import { TicketItem } from '@/features/ticket/components/ticket-item';
-import { ticketPath } from '@/path';
-import { LucideCheckCircle, LucideFileText, LucidePencil } from 'lucide-react';
-import Link from 'next/link';
+import { getTickets } from '@/features/ticket/queries/get-tickets';
+import { Ticket } from '@/features/ticket/types';
+import { useEffect, useState } from 'react';
 
-const TICKET_ICONS = {
-  OPEN: <LucideFileText />,
-  DONE: <LucideCheckCircle />,
-  IN_PROGRESS: <LucidePencil />,
-};
+const TicketsPage = async () => {
+  const tickets = await getTickets();
 
-const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
-        <Heading title='Tickets' description='All your ticktes at one place' />
+      <Heading
+        title="Tickets"
+        description="All your ticktes at one place"
+      />
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
-        {intitialTickets.map((ticket) => (
-          <TicketItem key={ticket.id} ticket={ticket}/>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket.id} ticket={ticket} />
         ))}
       </div>
     </div>
