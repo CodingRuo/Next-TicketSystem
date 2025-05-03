90c522aa5d69b7553d04979da555539c855f6abb
René Schober <r.schober@outlook.com>
2025-05-03 06:58:32 +0200

    Adding Placeholder Component

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,4 +1,8 @@
+import { Placeholder } from "@/components/placeholder";
+import { Button } from "@/components/ui/button";
 import { intitialTickets } from "@/data";
+import { ticketsPath } from "@/path";
+import Link from "next/link";
 
 type TicketsPageProps = {
     params: Promise<{
@@ -11,7 +15,16 @@ const TicketPage = async ({ params }: TicketsPageProps) => {
     const ticket = intitialTickets.find((ticket) => ticket.id === ticketId);
 
     if (!ticket) {
-        return <div>Ticket not found!</div>;
+        return (
+            <Placeholder 
+                label="Ticket not found"
+                button={
+                    <Button asChild variant="outline">
+                        <Link href={ticketsPath()}>Go to Ticktes</Link>
+                    </Button>
+                }
+            />
+        );
     }
 
     return <div>
