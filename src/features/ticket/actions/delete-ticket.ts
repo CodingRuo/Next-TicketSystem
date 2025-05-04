d3f98d48e54635e5934edb7f5d86052143d0dd1c
René Schober <r.schober@outlook.com>
2025-05-04 08:05:37 +0200

    Create Redirect to tickets Page, after delete Ticket

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -1,11 +1,15 @@
 "use server";
 
 import { prisma } from "@/lib/prisma";
+import { ticketsPath } from "@/path";
+import { redirect } from "next/navigation";
 
 export const deleteTicket = async (id: string) => {
     await prisma.ticket.delete({
         where: {
             id
         }
-    })
+    });
+
+    redirect(ticketsPath());
 }
