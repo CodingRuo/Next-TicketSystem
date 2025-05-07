2f9c96023deb65326c2a45490914ad37c6738c51
René Schober <r.schober@outlook.com>
2025-05-07 06:56:55 +0200

    Create Error Handling and Schema validation for creating a form.

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -1,23 +1,33 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Ticket } from "@/generated"
 import { upsertTicket } from "../actions/upsert-ticket";
+import { SubmitButton } from "@/components/form/submit-button";
+import { useActionState } from "react";
 
 type TicketUpsertFormProps = {
     ticket?: Ticket;
 }
 
 const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
+    const [actionState, action] = useActionState(
+        upsertTicket.bind(null, ticket?.id),
+        { message: "" }
+    );
+
     return (
-        <form action={upsertTicket.bind(null, ticket?.id)} className="flex flex-col gap-y-2">
+        <form action={action} className="flex flex-col gap-y-2">
             <Label htmlFor="title">Title</Label>
             <Input id="title" name="title" type="text" defaultValue={ticket?.title} />
 
             <Label htmlFor="content">Content</Label>
             <Input id="content" name="content" type="text" defaultValue={ticket?.content} />
 
-            <Button type="submit">{ticket ? "Edit" : "Create"}</Button>
+            <SubmitButton label={ticket ? "Edit" : "Create"} />
+
+            {actionState.message}
         </form>
     );
 }
