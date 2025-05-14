485f8ec0ae2612b3437fad0e9cdfd9c55c42f068
codingruo <r.schober@outlook.com>
2025-05-14 18:04:39 +0200

    feat: add user ownership checks to TicketItem component

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -18,13 +18,18 @@ import { TICKET_ICONS } from "../constants";
 import { TicketMoreMenu } from "./ticket-more-menu";
 import { ticketPath, ticketEditPath } from "@/path";
 import { TicketWithMetadata } from "../types/types";
+import { getAuth } from "@/features/auth/queries/get-auth";
+import { isOwner } from "@/features/auth/utils/is-owner";
 
 type TicketItemProps = {
     ticket: TicketWithMetadata;
     isDetail?: boolean;
 };
 
-const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
+const TicketItem = async ({ ticket, isDetail }: TicketItemProps) => {
+    const { user } = await getAuth();
+    const isTicketOwner = isOwner(user, ticket);
+
     const detailButton = (
         <Button variant="outline" size="icon" asChild>
             <Link prefetch href={ticketPath(ticket.id)}>
@@ -33,15 +38,15 @@ const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
         </Button>
     );
 
-    const editButton = (
+    const editButton = isTicketOwner ? (
         <Button variant="outline" size="icon" asChild>
             <Link prefetch href={ticketEditPath(ticket.id)}>
                 <LucidePencil className="h-4 w-4" />
             </Link>
         </Button>
-    );
+    ): null;
 
-    const moreMenu = (
+    const moreMenu =  isTicketOwner ? (
         <TicketMoreMenu
             ticket={ticket}
             trigger={
@@ -50,7 +55,7 @@ const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
                 </Button>
             }
         />
-    );
+    ) : null;
 
     return (
         <div
