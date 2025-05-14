611cfb1d28b5427f861f6717c2c23a75d0cea41c
codingruo <r.schober@outlook.com>
2025-05-14 14:48:12 +0200

    feat: update Header component to support async authentication and render navigation items based on user state

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,39 +5,45 @@ import { ThemeSwitcher } from "./theme/theme-switcher"
 import { buttonVariants } from "./ui/button"
 import { SubmitButton } from "./form/submit-button"
 import { signOut } from "@/features/auth/actions/sign-out"
+import { getAuth } from "@/features/auth/queries/get-auth"
 
-const Header = () => {
-    const navItems = (
+const Header = async () => {
+    const { user } = await getAuth();
+
+    const navItems = user ? (
         <>
             <Link href={ticketsPath()} className={buttonVariants({ variant: "outline" })}>Tickets</Link>
-            <Link href={signUpPath()} className={buttonVariants({ variant: "outline" })}>Sign Up</Link>
-            <Link href={signInPath()} className={buttonVariants({ variant: "default" })}>Sign In</Link>
             <form action={signOut}>
                 <SubmitButton label="Sign Out" icon={<LucideLogOut />} />
             </form>
+        </>
+    ) : (
+        <>
+            <Link href={signUpPath()} className={buttonVariants({ variant: "outline" })}>Sign Up</Link>
+            <Link href={signInPath()} className={buttonVariants({ variant: "default" })}>Sign In</Link>
         </>
     );
 
 
     return (
         <nav
-          className="
+            className="
             supports-backdrop-blur:bg-background/60
             fixed left-0 right-0 top-0 z-20
             border-b bg-background/95 backdrop-blur
             w-full flex py-2.5 px-5 justify-between
           "
         >
-          <div className="flex items-center gap-x-2">
-              <Link href={homePath()} className={buttonVariants({ variant: 'default' })}>
-                <LucideKanban />
-                <h1 className="text-lg font-semibold">TicketBounty</h1>
-              </Link>
-          </div>
-          <div className="flex items-center gap-x-2">
-            <ThemeSwitcher />
-            {navItems}
-          </div>
+            <div className="flex items-center gap-x-2">
+                <Link href={homePath()} className={buttonVariants({ variant: 'default' })}>
+                    <LucideKanban />
+                    <h1 className="text-lg font-semibold">TicketBounty</h1>
+                </Link>
+            </div>
+            <div className="flex items-center gap-x-2">
+                <ThemeSwitcher />
+                {navItems}
+            </div>
         </nav>
     )
 }
