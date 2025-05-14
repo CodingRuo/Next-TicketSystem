f023b0e04bbdba85a0909df288172aa9a82593d0
codingruo <r.schober@outlook.com>
2025-05-14 14:21:34 +0200

    feat: update SignUpForm to pre-fill input fields with action state data

--- src/features/auth/actions/sign-up.ts
+++ src/features/auth/actions/sign-up.ts
@@ -51,7 +51,8 @@ export const signUp = async (_actionState: ActionState, formData: FormData) => {
         if ( error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2002") {
             return toActionState(
                 "ERROR",
-                "Either email or username is already in use"
+                "Either email or username is already in use",
+                formData
             )
         }
 
