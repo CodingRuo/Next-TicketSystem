02836ffd6ea7f8d1b0bca6022b2e5203dd339138
René Schober <r.schober@outlook.com>
2025-05-15 17:36:15 +0200

    feat: enhance ticket search and pagination integration for improved user experience

--- src/features/ticket/components/ticket-pagination.tsx
+++ src/features/ticket/components/ticket-pagination.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { useQueryStates } from "nuqs";
-import { paginationOptions, paginationParser } from "../search-params";
+import { useQueryState, useQueryStates } from "nuqs";
+import { paginationOptions, paginationParser, searchParser } from "../search-params";
 import { Pagination } from "@/components/pagination";
+import { useEffect, useRef } from "react";
 
 type TicketPaginationProps = {
     paginatedTicketMetadata: {
@@ -13,6 +14,15 @@ type TicketPaginationProps = {
 
 const TicketPagination = ({ paginatedTicketMetadata }: TicketPaginationProps) => {
     const [pagination, setPagination] = useQueryStates(paginationParser, paginationOptions);
+    const [search] = useQueryState("search", searchParser);
+    const prevSearch = useRef(search);
+
+    useEffect(() => {
+        if (search ===  prevSearch.current) return;
+        prevSearch.current = search;
+
+        setPagination({ ...pagination, page: 0 });
+    }, [search, pagination, setPagination])
 
     return <Pagination pagination={pagination} onPagination={setPagination} paginatedMetadata={paginatedTicketMetadata}/>
 }
--- src/features/ticket/components/ticket-search-input.tsx
+++ src/features/ticket/components/ticket-search-input.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useQueryState } from "nuqs";
-import { searchParser } from "../search-params";
+import { useQueryState, useQueryStates } from "nuqs";
+import { paginationOptions, paginationParser, searchParser } from "../search-params";
 import { SearchInput } from "@/components/search-input";
 
 type SearchInputProps = {
@@ -10,11 +10,17 @@ type SearchInputProps = {
 
 const TicketSearchInput = ({ placeholder }: SearchInputProps) => {
     const [search, setSearch] = useQueryState("search", searchParser);
+    const [pagination, setPagination] = useQueryStates(paginationParser, paginationOptions);
+
+    const handleSearch = (value: string) => {
+        setPagination({ ...pagination, page: 0 });
+        setSearch(value);
+    }
 
     return (
         <SearchInput 
             value={search}
-            onChange={setSearch}
+            onChange={handleSearch}
             placeholder={placeholder}
         />
     );
