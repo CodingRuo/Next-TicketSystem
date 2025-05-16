49ecab95b21e1c422a700aec847ead1318d444b7
René Schober <r.schober@outlook.com>
2025-05-16 09:58:48 +0200

    feat: add comment deletion functionality with CommentDeleteButton component

---
+++ src/features/comment/actions/delete-comment.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { fromErrorToActionState, toActionState } from "@/components/form/utils/to-action-state";
+import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect";
+import { isOwner } from "@/features/auth/utils/is-owner";
+import { prisma } from "@/lib/prisma";
+import { ticketPath } from "@/path";
+import { revalidatePath } from "next/cache";
+
+export const deleteComment = async (id: string) => {
+    const { user } = await getAuthOrRedirect();
+
+    const comment = await prisma.comment.findUnique({
+        where: { id }
+    });
+
+    if (!comment || !isOwner(user, comment)) {
+        return toActionState("ERROR", "Not authorized");
+    }
+
+    try {
+        await prisma.comment.delete({
+            where: { id },
+        });
+    } catch (error) {
+        return fromErrorToActionState(comment.ticketId);
+    }
+
+    revalidatePath(ticketPath(comment.ticketId));
+
+    return toActionState("SUCCESS", "Comment deleted");
+}
