9102bcec969e4491c26a418c2ad34b5882d576a2
codingruo <r.schober@outlook.com>
2025-05-18 15:10:40 +0200

    feat: add "More" button to Comments component with click handler

--- src/features/comment/components/comments.tsx
+++ src/features/comment/components/comments.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import { CardCompact } from "@/components/card-compact";
 import { CommentItem } from "./comment-item";
 import { CommentCreateForm } from "./comment-create-form";
 import { CommentDeleteButton } from "./comment-delete-button";
 import { CommentWithMetadata } from "../types";
+import { Button } from "@/components/ui/button";
 
 type CommentsProps = {
     ticketId: string;
@@ -10,7 +13,9 @@ type CommentsProps = {
 }
 
 const Comments = ({ ticketId, comments }: CommentsProps) => {
-    console.log("Where am I displayed?")
+    const handleMore = () => {
+        console.log("More")
+    };
 
     return (
         <>
@@ -31,6 +36,14 @@ const Comments = ({ ticketId, comments }: CommentsProps) => {
                         ]}
                     />
                 ))}
+            </div>
+            <div className="flex flex-col justify-center ml-8">
+                <Button
+                    variant={"ghost"}
+                    onClick={handleMore}
+                >
+                    More
+                </Button>
             </div>
         </>
     )
