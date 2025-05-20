e9466774a2ad3f3eedbd46e729d72696a1e90c04
codingruo <r.schober@outlook.com>
2025-05-20 15:55:30 +0200

    feat: implement password reset functionality with form and validation

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,14 @@ import type { NextConfig } from "next";
 import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-            config.plugins = [...config.plugins, new PrismaPlugin()];
-        }
+    // /* config options here */
+    // webpack: (config, { isServer }) => {
+    //     if (isServer) {
+    //         config.plugins = [...config.plugins, new PrismaPlugin()];
+    //     }
 
-        return config;
-    },
+    //     return config;
+    // },
 };
 
 export default nextConfig;
---
+++ src/features/password/actions/password-forgot.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import {
+    ActionState,
+    fromErrorToActionState,
+    toActionState,
+} from "@/components/form/utils/to-action-state";
+import { prisma } from "@/lib/prisma";
+import { z } from "zod";
+
+const passwordForgotSchema = z.object({
+    email: z.string().min(1, { message: "Is required" }).max(191).email()
+});
+
+export const passwordForgot = async (_actionState: ActionState, formData: FormData) => {
+    try {
+        const { email } = passwordForgotSchema.parse(
+            Object.fromEntries(formData)
+        );
+
+        const user = await prisma.user.findUnique({
+            where: { email },
+        });
+
+        if (!user) {
+            return toActionState("ERROR", "Incorrect email.", formData);
+        }
+
+        // TODO: Send email with the reset link
+    } catch (error) {
+        return fromErrorToActionState(error, formData);
+    }
+
+    return toActionState("SUCCESS", "Check your email for a reset link");
+};
