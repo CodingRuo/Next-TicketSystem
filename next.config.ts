937b718d86bcfc930ab76590c21e7002ae29c0f3
codingruo <r.schober@outlook.com>
2025-05-20 14:13:47 +0200

    fix: add PrismaPlugin to Next.js configuration and update dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,16 @@
 import type { NextConfig } from "next";
+// @ts-ignore
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    /* config options here */
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()];
+        }
+
+        return config;
+    },
+};
 
 export default nextConfig;
