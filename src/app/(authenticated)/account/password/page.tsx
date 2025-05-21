71654221dc5fdc47debb9ca132a4867d348975cf
René Schober <r.schober@outlook.com>
2025-05-21 09:47:37 +0200

    feat: enhance password page layout with CardCompact component and improved structure

--- src/app/(authenticated)/account/password/page.tsx
+++ src/app/(authenticated)/account/password/page.tsx
@@ -1,14 +1,25 @@
-import { Heading } from "@/components/heading"
+import { CardCompact } from "@/components/card-compact";
+import { Heading } from "@/components/heading";
+import { PasswordForgotForm } from "@/features/password/components/password-forgot-form";
 import { AccountTabs } from "../_navigation/tabs";
 
 const PasswordPage = () => {
     return (
         <div className="flex-1 flex flex-col gap-y-8">
-            <Heading 
-                title="Password" 
-                description="Keep ypur account secure" 
+            <Heading
+                title="Password"
+                description="Keep ypur account secure"
                 tabs={<AccountTabs />}
             />
+
+            <div className="flex-1 flex flex-col items-center">
+                <CardCompact
+                    title="Change Password"
+                    description="Enter your current password"
+                    className="w-full max-w-[420px] animate-fade-from-top"
+                    content={<PasswordForgotForm />}
+                />
+            </div>
         </div>
     )
 }
