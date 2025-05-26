dd5abbb5fc86b32881ab15898ebc245c96fe86a5
codingruo <r.schober@outlook.com>
2025-05-26 22:20:51 +0200

    feat: verbessere E-Mail-Verifizierung mit Benutzerstatus-Updates und Cookie-Management
    - Aktualisiere den Benutzerstatus auf 'emailVerified' nach erfolgreicher Verifizierung.
    - Füge Cookie-Setzung zur Bestätigung der E-Mail-Verifizierung hinzu.
    - Integriere Validierung des E-Mail-Verifizierungscodes.

---
+++ src/features/auth/actions/email-verification-resend.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import {
+    fromErrorToActionState,
+    toActionState
+} from "@/components/form/utils/to-action-state";
+import { sendEmailVerification } from "../emails/send-email-verification";
+import { getAuthOrRedirect } from "../queries/get-auth-or-redirect";
+import { generateEmailVerificationCode } from "../utils/generate-email-verification-code";
+
+export const emailVerificationResend = async () => {
+    const { user } = await getAuthOrRedirect({
+        checkEmailVerified: false
+    });
+    try {
+
+        const verificationCode = await generateEmailVerificationCode(user.id, user.email);
+
+        const result = await sendEmailVerification(user.username, user.email, verificationCode);
+
+        if (result.error) {
+            return toActionState("ERROR", "Failed to send email verification code");
+        }
+
+        return toActionState("SUCCESS", "Email verification code resent");
+
+    } catch (error) {
+        return fromErrorToActionState(error);
+    }
+};
